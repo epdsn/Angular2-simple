97f3807406aa54bcaa8583c1d5ec76bfc8ee8b9e
epdsn <ibestudios@gmail.com>
2017-09-23 13:59:17 -0700

    Added some databinding, not really true 2 way binding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ export class AppComponent  {
   // [ ] means property binding - C to D
   // ( ) means event binding - D to C
   title = 'Eric\'s Angular 2 App';
-  name = 'Eric Perez'; 
+  name = 'Bob'; 
   ericsColor = 'blue';
 
   changeTextColor() {
