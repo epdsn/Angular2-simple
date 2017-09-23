f914b12bee790d6b2f6059824aaa505a9646ad7d
epdsn <ibestudios@gmail.com>
2017-09-23 13:44:16 -0700

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/app.component.html',
+})
+export class AppComponent  { 
+  // [ ] means property binding - C to D
+  // ( ) means event binding - D to C
+  title = 'Eric\'s Angular 2 App';
+  name = 'Eric Perez'; 
+  ericsColor = 'blue';
+
+  changeTextColor() {
+    this.ericsColor = this.ericsColor === 'blue' ? 'red' : 'blue';
+  }
+
+}
