93b44b40bf48a74601c0afffbb39e124318cdebb
epdsn <ibestudios@gmail.com>
2017-09-23 15:21:22 -0700

    Added Customer Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html',
+  templateUrl: 'app/app.component.html'
 })
 export class AppComponent  { 
   // [ ] means property binding - C to D
   // ( ) means event binding - D to C
   title = 'Eric\'s Angular 2 App';
   name = 'Bob'; 
-  ericsColor = 'blue';
+  ericsColor = 'green';
+  customers = [
+    { id: 1, name: 'Eric' },
+    { id: 2, name: 'Jake' },
+    { id: 3, name: 'Phillip' },
+    { id: 4, name: 'Sally' },
+    { id: 5, name: 'John' },
+  ];
 
   changeTextColor() {
-    this.ericsColor = this.ericsColor === 'blue' ? 'red' : 'blue';
+    this.ericsColor = this.ericsColor === 'green' ? 'red' : 'green';
   }
 
 }
