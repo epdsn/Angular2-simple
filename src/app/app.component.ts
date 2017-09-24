e35628b92077098b925ecd0af05e637c1d2354ed
epdsn <ibestudios@gmail.com>
2017-09-23 23:23:02 -0700

    added barrell(index) for customers.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-import { CustomerService } from './customer/customer.service';
 
+//import { CustomerService } from './customer/customer.service';
+//import { CustomersComponent } from './customer/customers.component'
+
+// here is the customer barrell, 
+// in the future you will be able to run ./customer and it will find index
+import { CustomerComponent, CustomerService } from './customer/index'
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html'
+  templateUrl: 'app/app.component.html',
+  providers: [CustomerService]
 })
 export class AppComponent  { 
   // [ ] means property binding - C to D
---
+++ src/app/customer/index.ts
@@ -0,0 +1,4 @@
+// a barrel of the customer related "stuff"
+export * from './customer.service';
+export * from './customer.component';
+export * from './customers.component';
