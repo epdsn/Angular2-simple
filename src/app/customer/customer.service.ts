de9aef0c687f4497819bef585a19d075b0dfb899
epdsn <ibestudios@gmail.com>
2017-09-23 22:32:25 -0700

    rx not working. :(

--- src/app/customer/customer.service.ts
+++ src/app/customer/customer.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
+import { Observable } from 'rxjs/Rx';
 
 const URL_CUSTOMER = 'app/customers.json';
 
 @Injectable()
 export class CustomerService {
-
     constructor( private _http : Http) { }
 
     getCustomers() {
         return this._http.get(URL_CUSTOMER)
-        .map((response : Response) =>  response.json());
+        .map((response : Response) =>  response.json())
+        .catch( this._handleError );
     }
+    
+    _handleError(err: any) {
+        console.log(err); // log this somewhere and format the message well for devs
+        return Observable // our opportunity customize this error
+    }
+    
 
 }
--- src/app/customer/customers.component.ts
+++ src/app/customer/customers.component.ts
@@ -10,13 +10,19 @@ import { Observable } from 'rxjs/Rx';
 export class CustomersComponent implements OnInit {
     
     myColor = 'grey';
-    customers: Observable<any[]>;
+    customers: Observable<Customer[]>;
 
     // shortcut in typescrip to create a private customer service object
     constructor( private _customerService: CustomerService ) {}
 
     ngOnInit() {
         // better here for setsting 
-        this.customers = this._customerService.getCustomers();
+        this.customers = this._customerService.getCustomers()
+            .catch((err) => {
+                console.log(err); // dont do this. Show user a nice message.
+                return Observable.of(true); 
+                // mow we eeat it, but only if the message 
+                // has been communicated to the user
+             } );
     }
 }
